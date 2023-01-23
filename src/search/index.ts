a2671627ad3584665081bc9eefafb149f28dba7d
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-23 00:12:06 +0000

    fix: get more specific about exports

--- src/search/index.ts
+++ src/search/index.ts
@@ -4,5 +4,5 @@ export {
   parseFilterQuery,
   createFilterTypes,
 } from './filters';
-export * from './query';
-export * from './sort';
+export { getFullTextSearchQuery } from './query';
+export { AscDescSort } from './sort';
