abdc96fc8c89f6a5e390536a0cac1429bede1640
Alex Mendes <alexanderhmendes@gmail.com>
2023-02-07 01:28:43 +0000

    fix: export IsUnique

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,3 +1,4 @@
 export { IsFilterSchema } from './is-filter-schema';
 export { IsSortSchema } from './is-sort-schema';
 export { IsIncludeSchema } from './is-include-schema';
+export { IsUnique } from './is-unique';
