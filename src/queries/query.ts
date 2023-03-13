d9414ab4ba9c735f9803b4e89d4ffdeeb8b9e352
Alex Mendes <alexanderhmendes@gmail.com>
2023-03-13 01:10:40 +0000

    fix: add multiple operators to getFullTextSearchQuery

--- src/queries/query.ts
+++ src/queries/query.ts
@@ -1,3 +1,9 @@
-export const getFullTextSearchQuery = (text: string) => ({
-  search: `${text.split(' ').join('|')}:*`,
+export const getFullTextSearchQuery = (
+  text: string,
+  joinWith: '&' | '|' | ' ' = ' ',
+) => ({
+  search: `${text
+    .split(' ')
+    .filter((x) => x)
+    .join(joinWith)}:*`,
 });
